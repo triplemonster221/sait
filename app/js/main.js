46d81453674e7162fec29f0441331034edc54d8d
triplemonster221 <Boorder1993@yandex.ru>
2022-07-24 12:15:27 +0500

    start project

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+const portfolioLend = new Swiper('.portfolio-lend', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.lend-next',
+      prevEl: '.lend-prev',
+    },
+  });
+  const portfolioShop = new Swiper('.portfolio-shop', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.shop-next',
+      prevEl: '.shop-prev',
+    },
+  });
+  const portfolioCorp = new Swiper('.portfolio-corp', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.corporat-next',
+      prevEl: '.corporat-prev',
+    },
+  });
